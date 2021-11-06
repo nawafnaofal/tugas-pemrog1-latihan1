c1ec2ba42ce5b5d9c45b86722398d902ef743ae4
nawafnaofal <nawafnaufal1@gmail.com>
2021-11-06 12:15:39 +0700

    melakukan perbaikan latihan 1

--- P1_3_1204036/Program.cs
+++ P1_3_1204036/Program.cs
@@ -11,14 +11,21 @@ namespace P1_3_1204036
         static void Main(string[] args)
         {
             
-                Console.Write("ANAK AYAM TURUN 10");
-                Console.WriteLine("");
-            for (int i = 10; i > 1; i=i-1)
-            {
-                Console.WriteLine("Anak ayam turunlah "+Convert.ToString(i)+" mati satu tingallah "+Convert.ToString(i - 1));
-            }
-            Console.Write("Anak ayam turunlah 1, mati satu tinggal induknya");
+            Console.Clear(); 
+            Console.Write("ANAK AYAM TURUN ");
+            int hayam = Convert.ToInt16(Console.ReadLine());
             Console.WriteLine("");
+            
+            for (int i = hayam; hayam > 1; i = i - 1)
+            {
+                if (i > 1 )
+                {
+                    Console.WriteLine("Anak ayam turunlah " + Convert.ToString(i) + " mati satu tingallah " + Convert.ToString(i - 1));
+                } else if(i == 1)                
+                {
+                    Console.Write("Anak ayam turunlah 1, mati satu tinggal induknya");                    
+                }
+            }
         }
     }
 }
