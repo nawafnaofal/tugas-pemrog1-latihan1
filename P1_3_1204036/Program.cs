793a4459632b5555d641c00d0f6210bb0bb57993
nawafnaofal <nawafnaufal1@gmail.com>
2021-11-07 12:24:32 +0700

    diperbaiki lagi

--- P1_3_1204036/Program.cs
+++ P1_3_1204036/Program.cs
@@ -14,16 +14,33 @@ namespace P1_3_1204036
             Console.Clear(); 
             Console.Write("ANAK AYAM TURUN ");
             int hayam = Convert.ToInt16(Console.ReadLine());
+
+            int ayam = hayam;
             Console.WriteLine("");
-            
-            for (int i = hayam; hayam > 1; i = i - 1)
+
             {
-                if (i > 1 )
+                if (ayam > 10 )
                 {
-                    Console.WriteLine("Anak ayam turunlah " + Convert.ToString(i) + " mati satu tingallah " + Convert.ToString(i - 1));
-                } else if(i == 1)                
+                    Console.WriteLine("Harap Inputkan 1 - 10 !");
+                } else if(ayam < 1)                
                 {
-                    Console.Write("Anak ayam turunlah 1, mati satu tinggal induknya");                    
+                    Console.WriteLine("Harap Inputkan 1 - 10 !");
+                }
+                    else
+                    {
+                for (int i = ayam; i> 0; i--)
+                    {
+                      if(i > 1)
+                        {
+                            Console.WriteLine("Anak ayam turunlah " + Convert.ToString(i) + " mati satu tingallah " + Convert.ToString(i - 1));
+
+                        }
+                      else if(i == 1)
+                        {
+                            Console.Write("Anak ayam turunlah "+i+ ", mati satu tinggal induknya");                    
+                            Console.WriteLine("");
+                        }
+                    }                    
                 }
             }
         }
